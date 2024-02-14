e3193ac997010fb2ad245355d93873164e3b6a40
LAPTOP-3VFTEQGA\tryyang <tryyang2001@gmail.com>
2024-02-14 16:54:43 +0800

    initialize the project

---
+++ frontend/README.md
@@ -0,0 +1,14 @@
+## Getting started
+
+First, to run the project server, type the command:
+
+```
+yarn dev
+```
+
+To build the project and run in production env, do the following:
+
+```
+yarn build
+yarn start
+```
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Button } from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    // feel free to edit/remove this code
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-medium mt-4">
+        Frontend Management System{" "}
+        <span className="text-lime-500 font-semibold">Homepage</span>
+      </h1>
+      <p>This is a proof that NextUI is working :)</p>
+      <Button
+        color="primary"
+        onClick={(e) => {
+          alert("Button clicked");
+        }}
+        className="my-4"
+      >
+        Click Me
+      </Button>
+    </div>
+  );
+}
