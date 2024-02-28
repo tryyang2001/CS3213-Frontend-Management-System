71be11e1e606a690056ca753ba550f30b544e228
William Chau Wei Xuan <55905659+WillCWX@users.noreply.github.com>
2024-02-28 14:15:10 +0800

    Style file input
    Styles file input by hiding the primitive HTML element and using react's
    useRef to interact with the hidden input.
    
    This allows us to create and stlye a file input however we like.

--- frontend/src/app/user/FileInput.tsx
+++ frontend/src/app/user/FileInput.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button, Modal, ModalBody, ModalContent, Image, useDisclosure } from "@nextui-org/react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function FileInput({onFileChange} : {onFileChange: Function}) {
     const {isOpen, onOpen, onOpenChange} = useDisclosure();
@@ -18,6 +18,7 @@ export default function FileInput({onFileChange} : {onFileChange: Function}) {
 
 function ModalUpload ({isOpen, onOpenChange, onUpload} : {isOpen: boolean, onOpenChange: VoidFunction, onUpload: Function}) {
 
+    const inputRef = useRef<HTMLInputElement>(null);
     const [photo, setPhoto] = useState<string>(""); 
     const [file, setFile] = useState<File>();
     const onChangePhoto = (event : React.ChangeEvent<HTMLInputElement>) => {
@@ -28,6 +29,7 @@ function ModalUpload ({isOpen, onOpenChange, onUpload} : {isOpen: boolean, onOpe
             setPhoto(fileURL)
         } else {
             setPhoto("")
+            setFile(undefined)
         }
     }
 
@@ -35,6 +37,10 @@ function ModalUpload ({isOpen, onOpenChange, onUpload} : {isOpen: boolean, onOpe
       setFile(undefined);
       setPhoto("");
       onOpenChange()
+    }
+
+    const onChooseFile = () => {
+      inputRef.current?.click()
     }
 
     const handleUpload = () => {
@@ -45,11 +51,12 @@ function ModalUpload ({isOpen, onOpenChange, onUpload} : {isOpen: boolean, onOpe
         setFile(undefined);
     }
 
-    return <Modal isOpen={isOpen} onOpenChange={onOpenChangeModal}>
+    return <Modal isOpen={isOpen} onOpenChange={onOpenChangeModal} size="2xl">
     <ModalContent className="pt-12">
       {(onClose) => (
         <>
           <ModalBody className="flex flex-col items-center">
+            {/* I can't get Avatar to limit image size to 200px */}
             {photo? <Image
                 removeWrapper
                 alt="Profile Picture"
@@ -60,8 +67,10 @@ function ModalUpload ({isOpen, onOpenChange, onUpload} : {isOpen: boolean, onOpe
             />
             : ""
             }
-            <div className="flex flex-row">
-                <input type="file" onChange={onChangePhoto}></input>
+            <div className="flex flex-row items-center gap-4">
+                <Button onClick={onChooseFile}> Choose File </Button>
+                <p>{file ? file.name : "Choose a profile picture"}</p>
+                <input type="file" className="hidden" onChange={onChangePhoto} ref={inputRef}></input>
                 <Button onClick={handleUpload}> upload </Button>
             </div>
           </ModalBody>
