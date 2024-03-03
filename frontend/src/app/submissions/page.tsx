40c9a9eb16c522a3410d8336f81afa0d9b5ff192
vivienherq <heruoqingvivien@gmail.com>
2024-03-03 23:40:56 +0800

    Create submissions page

--- frontend/src/app/submissions/page.tsx
+++ frontend/src/app/submissions/page.tsx
@@ -1,11 +1,6 @@
 "use client";
 
 import {
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  Image,
   Spacer,
   Accordion,
   AccordionItem,
@@ -18,7 +13,7 @@ import {
   Button,
 } from "@nextui-org/react";
 
-const DashBoard = () => {
+const Submissions = () => {
   const list = [
     {
       title: "Assignment 1",
@@ -44,63 +39,80 @@ const DashBoard = () => {
 
   const submissions = [
     {
+      id: "1",
       assignment: "Assignment 1",
       date: new Date(2024, 1, 28, 23, 59, 0),
     },
     {
+      id: "2",
       assignment: "Assignment 1",
       date: new Date(2024, 1, 29, 23, 59, 0),
     },
     {
+      id: "3",
       assignment: "Assignment 2",
       date: new Date(2024, 2, 28, 23, 59, 0),
     },
     {
+      id: "4",
       assignment: "Assignment 2",
       date: new Date(2024, 2, 29, 23, 59, 0),
     },
     {
+      id: "5",
       assignment: "Assignment 3",
       date: new Date(2024, 3, 28, 23, 59, 0),
     },
     {
+      id: "6",
       assignment: "Assignment 4",
       date: new Date(2024, 4, 28, 23, 59, 0),
     },
     {
+      id: "7",
       assignment: "Assignment 5",
       date: new Date(2024, 5, 28, 23, 59, 0),
     },
   ];
+
+  const columns = [
+    {
+      key: "submission",
+      label: "Submission Date and Time",
+    },
+    {
+      key: "view",
+      label: "Click to View",
+    },
+  ];
+
   return (
     <div className="m-4">
-      <b>Assignments</b>
+      <b>Submissions</b>
       <Spacer y={4} />
       <Accordion variant="splitted">
         {list.map((item, index) => (
-          <AccordionItem
-            key={index}
-            aria-label={item.title}
-            title={item.title}
-            // className="bg-blue-200 text-blue-800"
-          >
-            <Button size="md">Continue</Button>
-            <Table aria-label="Example static collection table">
+          <AccordionItem key={index} aria-label={item.title} title={item.title}>
+            <Table isStriped aria-label={`Submissions table for ${item.title}`}>
               <TableHeader>
-                <TableColumn>DATE</TableColumn>
-                <TableColumn>VIEW</TableColumn>
+                <TableColumn>Submission Date and Time</TableColumn>
+                <TableColumn width="20" align="end">
+                  Click to View
+                </TableColumn>
               </TableHeader>
-              <TableBody>
-                {submissions
-                  .filter((submission) => submission.assignment === item.title)
-                  .map((submission, index) => (
-                    <TableRow key={index}>
-                      <TableCell>{submission.date.toLocaleString()}</TableCell>
-                      <TableCell>
-                        <Button color="primary">Click to view</Button>
-                      </TableCell>
-                    </TableRow>
-                  ))}
+              <TableBody
+                items={submissions.filter(
+                  (submission) => submission.assignment === item.title
+                )}
+              >
+                {(submission) => (
+                  <TableRow key={submission.id}>
+                    <TableCell>{submission.date.toLocaleString()}</TableCell>
+                    <TableCell>
+                      <Button color="primary">View</Button>
+                    </TableCell>
+                  </TableRow>
+                )}
               </TableBody>
             </Table>
           </AccordionItem>
@@ -110,4 +122,4 @@ const DashBoard = () => {
   );
 };
 
-export default DashBoard;
+export default Submissions;
