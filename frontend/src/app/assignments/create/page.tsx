e28137378b70d09befe586f866ac5b02eddfce17
Jun Hong <junhongloww@gmail.com>
2024-03-18 00:59:15 +0800

    Modify components to use new form

--- frontend/src/app/assignments/create/page.tsx
+++ frontend/src/app/assignments/create/page.tsx
@@ -1,53 +1,14 @@
 "use client";
 
-import { FileUpload } from "@/components/FileUpload";
-import { Button, Input } from "@nextui-org/react";
-import { useState } from "react";
+import AssignmentCreator from "@/components/forms/AssignmentCreator";
 
 export default function Create() {
 
-  const [numQuestions, setNumQuestions] = useState<number>(1);
-  
-  const handleNumQuestionsChange = (value: number) => {
-    setNumQuestions(value);
-  }
-  
-  const renderQuestionUploads = () => {
-    const uploads = [];
-    for (let i = 0; i < numQuestions; i++) {
-      uploads.push(
-        <div className="gap-12 pt-10 flex" key={i}>
-          <div className="w-1/2 overflow-auto max-h-60">
-            <h2 className="font-bold underline">Question {i + 1}</h2>
-            <FileUpload expectedFileTypes={['html']}/>
-          </div>
-          <div className="w-1/2 overflow-auto max-h-60">
-            <h2 className="font-bold underline">Suggested Soution {i + 1}</h2>
-            <FileUpload expectedFileTypes={['py', 'java']}/>
-          </div>
-        </div>
-      );
-    }
-    return uploads;
-  };
 
     return (
-      <div className="columns-auto p-12">
-        <h1 className="font-bold text-4xl pb-3">Assignment Creation</h1>
-        <Input label="Assignment Title"></Input>
-        <div className="gap-12 pt-3 flex">
-            <Input
-              className="w-1/3"
-              label="Number of questions"
-              type="number"
-              defaultValue={numQuestions.toString()}
-              min={1}
-              onChange={(e) => handleNumQuestionsChange(parseInt(e.target.value))}
-            />
-            <Input className="w-1/3" label="Deadline" type="datetime-local"/>
-            <Button className="w-1/3" color="primary">Create Assignment</Button>
+        <div className="columns-auto items-center p-12">
+            <h1 className="font-bold text-4xl text-center">Assignment Creation</h1>
+            {AssignmentCreator()}
         </div>
-          {renderQuestionUploads()}
-      </div>
     );
-  }
+}
--- frontend/src/components/FileUpload.tsx
+++ frontend/src/components/FileUpload.tsx
@@ -3,79 +3,84 @@ import parse from "html-react-parser";
 import DateUtils from "@/utils/dateUtils";
 
 interface Props {
-  expectedFileTypes: string[];
+    expectedFileTypes: string[];
+    questionIndex: number;
+    type: string;
+    onFileUpload: (questionIndex: number, type: string, fileContent: string) => void;
 }
 
-export const FileUpload = ({expectedFileTypes}: Props) => {
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
-  const [fileContent, setFileContent] = useState<string | null>(null);
-  const [isValidFile, setIsValidFile] = useState<boolean>(true);
+export const FileUpload = ({ expectedFileTypes, questionIndex, type, onFileUpload }: Props) => {
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+    const [fileContent, setFileContent] = useState<string | null>(null);
+    const [isValidFile, setIsValidFile] = useState<boolean>(true);
 
-  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-        setSelectedFile(file);
-        file.text().then((fileContent) => {
-          setFileContent(fileContent);
-        });
-    } else {
-      setSelectedFile(null);
-      setFileContent(null);
-    }
-  };
-
-  function getExtension(filename: string) {
-    var parts = filename.split('.');
-    return parts[parts.length - 1];
-  }
-
-  useEffect(() => {
-    if (selectedFile && fileContent) {
-      var extension = getExtension(selectedFile?.name);
-      if (!expectedFileTypes.includes(extension)) {
-        setIsValidFile(false);
-      } else {
-        setIsValidFile(true);
-      }
-    }
-  }, [fileContent, expectedFileTypes]);
-
-  const renderPreview = () => {
-    if (isValidFile) {
-      if (selectedFile && fileContent) {
-        var extension = getExtension(selectedFile?.name);
-        let content;
-        if (extension === 'html') {
-          content = parse(fileContent);
-        } else if (extension === 'py' || extension === 'java') {
-          content = <pre>{fileContent}</pre>;
+    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            setSelectedFile(file);
+            file.text().then((fileContent) => {
+                setFileContent(fileContent);
+            });
+        } else {
+            setSelectedFile(null);
+            setFileContent(null);
         }
-        return (
-          <div>
-            {content}
-            <p>File Name: {selectedFile.name}</p>
-            <p>Last Modified: {DateUtils.parseTimestampToDate(selectedFile.lastModified)}</p>
-          </div>
-        )
-      } 
-      return <div>Select a file to upload</div>;
-    } else {
-      return (
-        <div>
-          Invalid file type.<br/>
-          Please upload a file with one of the following types: {expectedFileTypes.join(",")}
-        </div>
-      );
-    }
-  };
+    };
 
-  return (
-    <div>
-      {renderPreview()}
-      <input
-        type="file"
-        onChange={handleFileChange}
-      />
-    </div>
-  );
+    function getExtension(filename: string) {
+        var parts = filename.split('.');
+        return parts[parts.length - 1];
+    }
+
+    useEffect(() => {
+        if (selectedFile && fileContent) {
+            var extension = getExtension(selectedFile?.name);
+            if (!expectedFileTypes.includes(extension)) {
+                setIsValidFile(false);
+                onFileUpload(questionIndex, type, "");
+            } else {
+                setIsValidFile(true);
+                onFileUpload(questionIndex, type, fileContent);
+            }
+        }
+    }, [fileContent]);
+
+    const renderPreview = () => {
+        if (isValidFile) {
+            if (selectedFile && fileContent) {
+                var extension = getExtension(selectedFile?.name);
+                let content;
+                if (extension === 'html') {
+                    content = parse(fileContent);
+                } else if (extension === 'py' || extension === 'java') {
+                    content = <pre>{fileContent}</pre>;
+                }
+                return (
+                    <div>
+                        {content}
+                        <p>File Name: {selectedFile.name}</p>
+                        <p>Last Modified: {DateUtils.parseTimestampToDate(selectedFile.lastModified)}</p>
+                    </div>
+                )
+            }
+            return <div>Select a file to upload</div>;
+        } else {
+            return (
+                <div>
+                    Invalid file type.<br />
+                    Please upload a file with one of the following types: {expectedFileTypes.join(",")}
+                </div>
+            );
+        }
+    };
+
+    return (
+        <div>
+            {renderPreview()}
+            <input
+                type="file"
+                onChange={handleFileChange}
+            />
+        </div>
+    );
 };
