0b4b8493d04abf38426b2e976173f45793640099
Jun Hong <junhongloww@gmail.com>
2024-03-18 20:21:49 +0800

    Modify functional component to be called as component

--- frontend/src/app/assignments/create/page.tsx
+++ frontend/src/app/assignments/create/page.tsx
@@ -8,7 +8,7 @@ export default function Create() {
     return (
         <div className="columns-auto items-center p-12">
             <h1 className="font-bold text-4xl text-center">Assignment Creation</h1>
-            {AssignmentCreator()}
+            <AssignmentCreator />
         </div>
     );
 }
