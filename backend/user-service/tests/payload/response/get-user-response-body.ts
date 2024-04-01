257030d20db1adb1c1e6cb588c8d2e4123bf5ee5
Jun Hong <junhongloww@gmail.com>
2024-04-02 00:05:46 +0800

    Add unit tests for /user/getUserByUserId endpoint

---
+++ backend/user-service/tests/payload/response/get-user-response-body.ts
@@ -0,0 +1,11 @@
+export const getGetUserResponseBody = () => {
+    return {
+        uid: 1,
+        email: 'test@example.com',
+        password: 'password12345',
+        name: 'Test',
+        major: 'Computer Science',
+        course: 'CS1101S',
+        role: 'student',
+    };
+  };
