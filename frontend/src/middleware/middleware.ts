3454cbcf693e99c63dc328dc57570b91989f2efe
hhchinh2002 <hhchinh2002@gmail.com>
2024-04-07 21:22:25 +0800

    Fixing middleware.ts in frontend

--- frontend/src/middleware/middleware.ts
+++ frontend/src/middleware/middleware.ts
@@ -7,7 +7,7 @@ export const config = {
   matchers: "/:path*",
 };
 
-export default function middleware(request: NextRequest) {
+export default async function middleware(request: NextRequest) {
   const publicRoutes = ["/_next", "/public"];
   const loggedInRequiredRoutes = ["/user", "/assignments", "/dashboard"];
   const redirectRoutes = ["/"];
@@ -20,20 +20,14 @@ export default function middleware(request: NextRequest) {
     return NextResponse.next();
   }
 
-  // TODO: check if user is authenticated, and redirect to login page if not
   if (loggedInRequiredRoutes.some((route) => path.startsWith(route))) {
-    if (user) {
-        const validated = await userService.validateUser(user.uid);
-        if (!validated) {
-            toast.error("Fail to validate your identity, you need to login again");
-            return NextResponse.redirect(new URL("/login"));
-        }
-    } else {
+    if (!user) {
         toast.error("You must login first to access this page");
         return NextResponse.redirect(new URL("/login"));
     }
     return NextResponse.next();
   }
+  
   // redirect to dashboard page if home page is accessed
   if (redirectRoutes.includes(path)) {
     return NextResponse.redirect(new URL("/dashboard", request.nextUrl.origin));
